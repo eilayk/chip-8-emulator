ab866b2738a786bf8c56076d9c5160c011c79bed
Eilay K <22532056+eilayk@users.noreply.github.com>
2025-12-22 16:10:10 -0500

    fix(wasm-pkg): add wasm package to repo

---
+++ web/pkg/web_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_chip8wasm_free: (a: number, b: number) => void;
+export const chip8wasm_cycle: (a: number) => void;
+export const chip8wasm_get_display: (a: number) => [number, number];
+export const chip8wasm_load_rom: (a: number, b: number, c: number) => void;
+export const chip8wasm_new: () => number;
+export const chip8wasm_set_pressed_keys: (a: number, b: number, c: number) => void;
+export const chip8wasm_tick_timers: (a: number) => void;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_start: () => void;
